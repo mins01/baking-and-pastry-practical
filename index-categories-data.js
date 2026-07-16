24ef108a3142c920c2a713beb42580ea677a0807
공대여자 <mins01.lycos.co.kr@gmail.com>
2026-07-16 21:28:21 +0900

    데이터 연동 변경

--- index-categories-data.js
+++ index-categories-data.js
@@ -1,33 +1,14 @@
 const categories = {
   pastry: {
     label: '제과',
-    questions: [
-
-    ],
+    questions: Object.entries(questions).filter(([key, question]) => key.startsWith('pastry_') ).map(([key, question]) => {
+      return { key, number: question.number, label: question.label, };
+    }),
   },
   baking: {
     label: '제빵',
-    questions: [
-      { key: 'baking_1', number: 1, label: '빵도넛', },
-      { key: 'baking_2', number: 2, label: '소시지빵', },
-      { key: 'baking_3', number: 3, label: '식빵(비상스트레이트법)', },
-      { key: 'baking_4', number: 4, label: '단팥빵(비상스트레이트법)', },
-      { key: 'baking_5', number: 5, label: '그리시니', },
-      { key: 'baking_6', number: 6, label: '밤식빵', },
-      { key: 'baking_7', number: 7, label: '베이글', },
-      { key: 'baking_8', number: 8, label: '스위트롤', },
-      { key: 'baking_9', number: 9, label: '우유식빵', },
-      { key: 'baking_10', number: 10, label: '단과자빵(트위스트형)', },
-      { key: 'baking_11', number: 11, label: '단과자빵(크림빵)', },
-      { key: 'baking_12', number: 12, label: '풀만식빵', },
-      { key: 'baking_13', number: 13, label: '단과자빵(소보로빵)', },
-      { key: 'baking_14', number: 14, label: '쌀식빵', },
-      { key: 'baking_15', number: 15, label: '호밀빵', },
-      { key: 'baking_16', number: 16, label: '버터톱식빵', },
-      { key: 'baking_17', number: 17, label: '옥수수식빵', },
-      { key: 'baking_18', number: 18, label: '모카빵', },
-      { key: 'baking_19', number: 19, label: '버터롤', },
-      { key: 'baking_20', number: 20, label: '통밀빵', },
-    ],
+    questions: Object.entries(questions).filter(([key, question]) => key.startsWith('baking_') ).map(([key, question]) => {
+      return { key, number: question.number, label: question.label, };
+    }),
   }
 }
